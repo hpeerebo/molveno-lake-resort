5295ac269121a0f6f25725b0e1193069c1747104
Powercommy <sander@jemag.nl>
2019-02-12 14:27:12 +0100

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from "@angular/forms";
 import { HomeComponent } from './components/home/home.component';
+import { ActiviteitenComponent } from './components/activiteiten/activiteiten.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { KamersComponent } from './components/kamers/kamers.component';
+import { LoginComponent } from './components/login/login.component';
+import { RestaurantComponent } from './components/restaurant/restaurant.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ActiviteitenComponent,
+    ContactComponent,
+    KamersComponent,
+    LoginComponent,
+    RestaurantComponent
   ],
   imports: [
     BrowserModule,
