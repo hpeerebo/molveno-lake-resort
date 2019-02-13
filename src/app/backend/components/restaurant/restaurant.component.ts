d0b6228ee4f2a3b3f648a0610ddb9b22a88cbde0
SanderHuijbers <sander.huijbers@kpn.com>
2019-02-13 15:42:41 +0100

    Added a list of tables to restaurant.

--- src/app/backend/components/restaurant/restaurant.component.ts
+++ src/app/backend/components/restaurant/restaurant.component.ts
@@ -6,10 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./restaurant.component.scss']
 })
 export class BackEndRestaurantComponent implements OnInit {
-
+  tafels: any[] = [
+    { tafelNummer: 1, aantalPersonen: 4 },
+    { tafelNummer: 2, aantalPersonen: 4 },
+    { tafelNummer: 3, aantalPersonen: 2 },
+    { tafelNummer: 4, aantalPersonen: 4 },
+    { tafelNummer: 5, aantalPersonen: 4 },
+    { tafelNummer: 6, aantalPersonen: 6 },
+    { tafelNummer: 7, aantalPersonen: 2 },
+    { tafelNummer: 8, aantalPersonen: 4 },
+    { tafelNummer: 9, aantalPersonen: 4 },
+    { tafelNummer: 10, aantalPersonen: 2 },
+    { tafelNummer: 11, aantalPersonen: 4 },
+    { tafelNummer: 12, aantalPersonen: 4 },
+    { tafelNummer: 13, aantalPersonen: 6 },
+    { tafelNummer: 14, aantalPersonen: 2 },
+    { tafelNummer: 15, aantalPersonen: 2 }
+  ];
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+class Tafel {
+  public tafelNummer: number;
+  public aantalPersonen: number;
+
+  constructor(tafelNummer: number, aantalPersonen: number) {
+      this.tafelNummer = tafelNummer;
+      this.aantalPersonen = aantalPersonen;
+  }
 }
