fa6cd5639b6e111254820683a46bf547b479633b
Jilles <jilles.wielaard@kpn.com>
2019-02-13 14:05:57 +0100

    changes quotes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from "@angular/forms";
 import {FrontendModule} from "./frontend/frontend.module";
-import {BackendModule} from "./backend/backend.module";
+import {BackendModule} from './backend/backend.module';
 
 @NgModule({
   declarations: [
