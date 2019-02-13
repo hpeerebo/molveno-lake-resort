47b470a2fa64d4b7f4a35e085601752bc2901986
Jos Roubos <j.j.roubos@kpn.com>
2019-02-13 23:29:30 +0100

    inhoud toegevoegd

--- src/app/backend/components/restaurant/ingredienten/ingredienten.component.ts
+++ src/app/backend/components/restaurant/ingredienten/ingredienten.component.ts
@@ -5,11 +5,28 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './ingredienten.component.html',
   styleUrls: ['./ingredienten.component.scss']
 })
-export class IngredientenComponent implements OnInit {
+export class IngredientenComponent {
 
-  constructor() { }
+  backendData = [
+    { naam: "Aardappelen", soort: "nog te bepalen", prijs: 13.50 },
+    { naam: "Bleekselderij", soort: "nog te bepalen", prijs: 13.50 },
+    { naam: "Broccoli", soort: "nog te bepalen", prijs: 13.50 },
+    { naam: "Courgette", soort: "nog te bepalen", prijs: 13.50 },
+    { naam: "Fazant", soort: "nog te bepalen", prijs: 13.50 },
+    { naam: "Forel", soort: "nog te bepalen", prijs: 13.50 },
+    { naam: "Kaas", soort: "nog te bepalen", prijs: 13.50 },
+    { naam: "Knoflook", soort: "nog te bepalen", prijs: 13.50 },
+    { naam: "Krab", soort: "nog te bepalen", prijs: 13.50 },
+    { naam: "Linzen", soort: "nog te bepalen", prijs: 13.50 },
+    { naam: "Prei", soort: "nog te bepalen", prijs: 13.50 },
+    { naam: "Runderhaas", soort: "nog te bepalen", prijs: 13.50 },
+    { naam: "Venkel", soort: "nog te bepalen", prijs: 13.50 }
+  ];
 
-  ngOnInit() {
-  }
+  ingredienten: Ingredient[] = this.backendData.map(data => new Ingredient(data.naam, data.soort, data.prijs));
 
+}
+
+class Ingredient {
+  constructor(public naam: string, public soort: string, public prijs: number) { }
 }
