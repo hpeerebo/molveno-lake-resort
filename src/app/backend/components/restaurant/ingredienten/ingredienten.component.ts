fe84f462db25d2c648dbd12399f4e4acbd264bb3
Jos Roubos <j.j.roubos@kpn.com>
2019-02-13 23:39:21 +0100

    double quote vervangen door single quote

--- src/app/backend/components/restaurant/ingredienten/ingredienten.component.ts
+++ src/app/backend/components/restaurant/ingredienten/ingredienten.component.ts
@@ -8,19 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class IngredientenComponent {
 
   backendData = [
-    { naam: "Aardappelen", soort: "nog te bepalen", prijs: 13.50 },
-    { naam: "Bleekselderij", soort: "nog te bepalen", prijs: 13.50 },
-    { naam: "Broccoli", soort: "nog te bepalen", prijs: 13.50 },
-    { naam: "Courgette", soort: "nog te bepalen", prijs: 13.50 },
-    { naam: "Fazant", soort: "nog te bepalen", prijs: 13.50 },
-    { naam: "Forel", soort: "nog te bepalen", prijs: 13.50 },
-    { naam: "Kaas", soort: "nog te bepalen", prijs: 13.50 },
-    { naam: "Knoflook", soort: "nog te bepalen", prijs: 13.50 },
-    { naam: "Krab", soort: "nog te bepalen", prijs: 13.50 },
-    { naam: "Linzen", soort: "nog te bepalen", prijs: 13.50 },
-    { naam: "Prei", soort: "nog te bepalen", prijs: 13.50 },
-    { naam: "Runderhaas", soort: "nog te bepalen", prijs: 13.50 },
-    { naam: "Venkel", soort: "nog te bepalen", prijs: 13.50 }
+    { naam: 'Aardappelen', soort: 'nog te bepalen', prijs: 13.50 },
+    { naam: 'Bleekselderij', soort: 'nog te bepalen', prijs: 13.50 },
+    { naam: 'Broccoli', soort: 'nog te bepalen', prijs: 13.50 },
+    { naam: 'Courgette', soort: 'nog te bepalen', prijs: 13.50 },
+    { naam: 'Fazant', soort: 'nog te bepalen', prijs: 13.50 },
+    { naam: 'Forel', soort: 'nog te bepalen', prijs: 13.50 },
+    { naam: 'Kaas', soort: 'nog te bepalen', prijs: 13.50 },
+    { naam: 'Knoflook', soort: 'nog te bepalen', prijs: 13.50 },
+    { naam: 'Krab', soort: 'nog te bepalen', prijs: 13.50 },
+    { naam: 'Linzen', soort: 'nog te bepalen', prijs: 13.50 },
+    { naam: 'Prei', soort: 'nog te bepalen', prijs: 13.50 },
+    { naam: 'Runderhaas', soort: 'nog te bepalen', prijs: 13.50 },
+    { naam: 'Venkel', soort: 'nog te bepalen', prijs: 13.50 }
   ];
 
   ingredienten: Ingredient[] = this.backendData.map(data => new Ingredient(data.naam, data.soort, data.prijs));
