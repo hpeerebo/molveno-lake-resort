8236cf1d91e8e0995cd9cf8339786350e2b61d29
Hafid Bourich <hafid.bourich@kpn.com>
2019-02-14 12:16:47 +0100

    meer code

--- src/app/backend/components/kamers/kamers.component.ts
+++ src/app/backend/components/kamers/kamers.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RoomsService, KamerResponse } from 'src/app/services/rooms.service';
+import { RoomsService, KamerResponse, Kamer } from 'src/app/services/rooms.service';
 
 @Component({
   selector: 'app-kamers',
@@ -7,12 +7,19 @@ import { RoomsService, KamerResponse } from 'src/app/services/rooms.service';
   styleUrls: ['./kamers.component.scss']
 })
 export class BackEndKamersComponent implements OnInit {
-  public kamers :KamerResponse[] = [];
+  //public kamers :KamerResponse[] = [];
+  public kamers :Kamer[] = [];
+  show = true;
+  variable = 1
  // constructor() { }
  constructor(private roomservice: RoomsService){
   
   roomservice.getRoom().subscribe(result => this.kamers = result);
+  //roomservice.getRoom().subscribe(result => this.kamers);
   //console.log(this.kamers);
+}
+showReservedRooms(){
+  this.kamers = [];
 }
 
   ngOnInit() {
