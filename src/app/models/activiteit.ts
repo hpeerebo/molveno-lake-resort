3b06c9ecf83bae9c27762f4efcaad266bc8ca9f3
IctQ <33573165+IctQ@users.noreply.github.com>
2019-02-25 16:13:13 +0100

    Revert "Feature actpieter"

--- src/app/models/activiteit.ts
+++
@@ -1,8 +0,0 @@
-export class Activiteit {
-  constructor(public activiteitNummer: number,
-              public activiteitNaam: string,
-              public activiteitDatum: string,
-              public aantalPersonen: number,
-              public prijs: number) {
-  }
-}
