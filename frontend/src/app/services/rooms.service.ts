d781e383f3dc5de775a511061e56d8c18d2c0d5e
Hafid Bourich <hafid.bourich@kpn.com>
2019-02-26 17:40:18 +0100

    feature kamer met proxy naar backend

--- frontend/src/app/services/rooms.service.ts
+++ frontend/src/app/services/rooms.service.ts
@@ -21,18 +21,20 @@ export class RoomService {
 
   getRoom(): Observable<Kamer[]> {
     const headers = new HttpHeaders({});
-    return this.http.get<KamerResponse>("http://www.mocky.io/v2/5c65473a3300005c11b99c33", {headers: headers})
-    //return this.http.get<KamerResponse>("http://localhost:3000/kamers", {headers: headers})
+   // return this.http.get<KamerResponse>("http://www.mocky.io/v2/5c65473a3300005c11b99c33", {headers: headers})
+   return this.http.get<KamerDetails>("/api/kamers", {headers: headers})
     .pipe(
-      map(RoomService.IOMDBResponseKamerToKamerMapper),
+     // map(RoomService.IOMDBResponseKamerToKamerMapper),
+     map((data: any) => data.map((kamer: Kamer) =>new Kamer(kamer.kamerNummer, kamer.kamerType, kamer.kamerLigging, kamer.aantalPersonen, kamer.prijs, kamer.status))),
+ //    map((res: any) => res.response.map((kamer: Kamer) => new Kamer(kamer.kamerNummer, kamer.kamerType, kamer.kamerLigging, kamer.aantalPersonen, kamer.prijs, kamer.status))),
     )
   }
 
   private static IOMDBResponseKamerToKamerMapper(kamerresponse: KamerResponse): Kamer[] {
     // return omdbSearchResponse.Search.map(omdbFilm => new Film(omdbFilm.Poster, omdbFilm.Title, omdbFilm.Type, omdbFilm.Year, omdbFilm.imdbID))
-    return kamerresponse.response.map(RoomService.KamerDetailsToFilm)
+    return kamerresponse.response.map(RoomService.KamerDetailsToKamer)
   }
-  private  static KamerDetailsToFilm(kamerdetails: KamerDetails) {
+  private  static KamerDetailsToKamer(kamerdetails: KamerDetails) {
     return new Kamer(kamerdetails.kamerNummer, kamerdetails.kamerType, kamerdetails.kamerLigging, kamerdetails.aantalPersonen, kamerdetails.prijs, kamerdetails.status)
   }
 
