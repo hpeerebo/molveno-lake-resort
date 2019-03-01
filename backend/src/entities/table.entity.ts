88fc2f283d63a3569c1a3b075f256a38c1a3569c
Jilles Wielaard <jilleswielaard@iMac-van-Jilles.local>
2019-03-01 13:48:46 +0100

    database connection changes

---
+++ backend/src/entities/table.entity.ts
@@ -0,0 +1,18 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class MovieRepoEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'varchar', length: 50 })
+    kenmerk: string;
+
+    @Column('int')
+    personen: number;
+
+    constructor(kenmerk: string, personen: number) {
+        this.kenmerk = kenmerk;
+        this.personen = personen;
+    }
+}
