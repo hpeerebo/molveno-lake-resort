4d1fda24c3044e328ea448f2ca451c3f7b1e581e
SanderHuijbers <sander.huijbers@kpn.com>
2019-03-08 17:05:38 +0100

    Restaurant menu in frontend weer hersteld.

--- frontend/src/app/frontend/components/restaurant/restaurant.component.ts
+++ frontend/src/app/frontend/components/restaurant/restaurant.component.ts
@@ -1,38 +1,38 @@
-import { GerechtenService } from 'src/app/services/gerechten.service';
-import { Component, OnInit } from '@angular/core';
-import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
-import { Observable } from 'rxjs';
-import { Gerecht } from 'src/app/models/gerecht';
+import { Component, OnInit } from "@angular/core";
+import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-  selector: 'app-restaurant',
-  templateUrl: './restaurant.component.html',
-  styleUrls: ['./restaurant.component.scss']
+  selector: "app-restaurant",
+  templateUrl: "./restaurant.component.html",
+  styleUrls: ["./restaurant.component.scss"]
 })
 export class RestaurantComponent implements OnInit {
-  images = ['assets/img/dishes/car1.jpg', 'assets/img/dishes/car2.jpg', 'assets/img/dishes/car3.jpg'];
-  public gerechten: Observable<Gerecht[]> = this.gerechtenService.getAllGerechten();
+  images = [
+    "assets/img/dishes/car1.jpg",
+    "assets/img/dishes/car2.jpg",
+    "assets/img/dishes/car3.jpg"
+  ];
+  closeResult = "";
+  constructor(private modalService: NgbModal) {}
 
-  constructor(private gerechtenService: GerechtenService, private modalService: NgbModal) {}
-
-  closeResult = '';
-
-    open(content: any) {
-    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
-      result => {
-        this.closeResult = `Closed with: ${result}`;
-      },
-      reason => {
-        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-      }
-    );
+  open(content: any) {
+    this.modalService
+      .open(content, { ariaLabelledBy: "modal-basic-title" })
+      .result.then(
+        result => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        reason => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
   }
 
   private getDismissReason(reason: any): string {
     if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
+      return "by pressing ESC";
     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
+      return "by clicking on a backdrop";
     } else {
       return `with: ${reason}`;
     }
