cf20c77fa1cce820e08028c20ce996688c54f7b1
Jilles Wielaard <jilleswielaard@iMac-van-Jilles.local>
2019-03-09 21:56:18 +0100

    update tafel and refactor

--- backend/src/controllers/restaurant/restaurant.controller.ts
+++ backend/src/controllers/restaurant/restaurant.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Delete, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Delete, Param, Put } from '@nestjs/common';
 import { Tafel } from 'src/models/tafel';
 import { TafelService } from 'src/services/tafel/tafel.service';
 import { CreateTafelDto } from 'src/dto/create-tafel-dto';
@@ -12,7 +12,7 @@ import { IngredientService } from 'src/services/ingredient/ingredient.service';
 import { Tafelreservering } from 'src/models/tafelreservering';
 import { TafelreserveringService } from 'src/services/tafelreservering/tafelreservering.service';
 import { CreateTafelreserveringDto } from 'src/dto/create-tafelreservering-dto';
-import { DeleteResult } from 'typeorm';
+import { DeleteResult, UpdateResult } from 'typeorm';
 
 @ApiUseTags('restaurant')
 @Controller('restaurant')
@@ -41,7 +41,19 @@ export class RestaurantController {
         return tafelEntity.mapToTafel();
     }
 
+    @Put('tafels/:id')
+    @ApiOperation({ title: 'Wijzig de gegevens van een bestaande tafel' })
+    @ApiResponse({ status: 200, description: 'De tafel is bijgewerkt' })
+    @ApiResponse({ status: 404, description: 'Een tafel met dit id is niet gevonden' })
+    @ApiResponse({ status: 409, description: 'Een tafel met dit kenmerk bestaat al' })
+    modifyTafel(@Param('id') id: number, @Body() tafelDto: CreateTafelDto): Promise<UpdateResult> {
+        return this.tafelService.updateTafel(id, tafelDto.mapToTafelEntity());
+    }
+
     @Delete('tafels/:id')
+    @ApiOperation({ title: 'Verwijder een tafel' })
+    @ApiResponse({ status: 200, description: 'De tafel is verwijderd' })
+    @ApiResponse({ status: 404, description: 'Een tafel met dit id is niet gevonden' })
     deleteTafel(@Param('id') id: number): Promise<DeleteResult> {
         return this.tafelService.deleteTafel(id);
     }
--- backend/src/services/tafel/tafel.service.ts
+++ backend/src/services/tafel/tafel.service.ts
@@ -3,32 +3,42 @@ import { Tafel } from 'src/models/tafel';
 import { CreateTafelDto } from 'src/dto/create-tafel-dto';
 import { TafelRepoEntity } from 'src/entities/tafel.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, DeleteResult } from 'typeorm';
+import { Repository, DeleteResult, UpdateResult, Not } from 'typeorm';
 
 @Injectable()
 export class TafelService {
+  constructor(
+    @InjectRepository(TafelRepoEntity)
+    private readonly tafelRepository: Repository<TafelRepoEntity>,
+  ) { }
 
-    constructor(
-        @InjectRepository(TafelRepoEntity)
-        private readonly tafelRepository: Repository<TafelRepoEntity>,
-    ) { }
+  async getTafels(): Promise<Tafel[]> {
+    const tafelEntities = await this.tafelRepository.find();
+    const tafels = tafelEntities.map(tafelEntity => tafelEntity.mapToTafel());
+    return tafels;
+  }
 
-    async getTafels(): Promise<Tafel[]> {
-        const tafelEntities = await this.tafelRepository.find();
-        const tafels = tafelEntities.map(tafelEntity => tafelEntity.mapToTafel());
-        return tafels;
-    }
+  async createTafel(tafelEntity: TafelRepoEntity): Promise<TafelRepoEntity> {
+    const tafelExists = !!(await this.tafelRepository.findOne({ kenmerk: tafelEntity.kenmerk }));
+    if (tafelExists) throw new HttpException('Een tafel met dit kenmerk bestaat al', HttpStatus.CONFLICT);
 
-    async createTafel(tafelEntity: TafelRepoEntity): Promise<TafelRepoEntity> {
-        const tafelExists = !!await this.tafelRepository.findOne({ where: { kenmerk: tafelEntity.kenmerk } });
-        if (!tafelExists) {
-            return this.tafelRepository.save(tafelEntity);
-        } else {
-            throw new HttpException('Een tafel met dit kenmerk bestaat al', HttpStatus.CONFLICT);
-        }
-    }
+    return this.tafelRepository.save(tafelEntity);
+  }
 
-    deleteTafel(id: number): Promise<DeleteResult> {
-        return this.tafelRepository.delete({id});
-    }
+  async updateTafel(id: number, tafelEntity: TafelRepoEntity): Promise<UpdateResult> {
+    const tafelExists = !!(await this.tafelRepository.findOne({ id }));
+    if (!tafelExists) throw new HttpException('Er bestaat geen tafel met dit id', HttpStatus.NOT_FOUND);
+
+    const kenmerkExists = !!(await this.tafelRepository.findOne({ id: Not(id), kenmerk: tafelEntity.kenmerk }));
+    if (kenmerkExists) throw new HttpException('Een tafel met dit kenmerk bestaat al', HttpStatus.CONFLICT);
+
+    return this.tafelRepository.update({ id }, tafelEntity);
+  }
+
+  async deleteTafel(id: number): Promise<DeleteResult> {
+    const tafelExists = !!(await this.tafelRepository.findOne({ id }));
+    if (!tafelExists) throw new HttpException('Er bestaat geen tafel met dit id', HttpStatus.NOT_FOUND);
+
+    return this.tafelRepository.delete({ id });
+  }
 }
