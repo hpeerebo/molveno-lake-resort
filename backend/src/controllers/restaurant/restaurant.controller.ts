88a0bc9385e783ff2fe12922fde35c4523540699
Jilles Wielaard <jilleswielaard@iMac-van-Jilles.local>
2019-03-09 22:24:27 +0100

    wijzig gerechten en ingredienten

--- backend/src/controllers/restaurant/restaurant.controller.ts
+++ backend/src/controllers/restaurant/restaurant.controller.ts
@@ -42,7 +42,7 @@ export class RestaurantController {
     }
 
     @Put('tafels/:id')
-    @ApiOperation({ title: 'Wijzig de gegevens van een bestaande tafel' })
+    @ApiOperation({ title: 'Wijzig de gegevens van een tafel' })
     @ApiResponse({ status: 200, description: 'De tafel is bijgewerkt' })
     @ApiResponse({ status: 404, description: 'Een tafel met dit id is niet gevonden' })
     @ApiResponse({ status: 409, description: 'Een tafel met dit kenmerk bestaat al' })
@@ -59,35 +59,67 @@ export class RestaurantController {
     }
 
     @Get('gerechten')
+    @ApiOperation({ title: 'Haal een lijst op van alle gerechten' })
     async getGerechten(): Promise<{ gerechten: Gerecht[] }> {
         const gerechten = await this.gerechtService.getGerechten();
         return { gerechten };
     }
 
     @Post('gerechten')
+    @ApiOperation({ title: 'Voeg een nieuw gerecht toe' })
+    @ApiResponse({ status: 201, description: 'Het gerecht is succesvol aangemaakt' })
+    @ApiResponse({ status: 409, description: 'Een gerecht met deze naam bestaat al' })
     async createGerecht(@Body() gerechtDto: CreateGerechtDto): Promise<Gerecht> {
         const gerechtEntity = await this.gerechtService.createGerecht(gerechtDto.mapToGerechtEntity());
         return gerechtEntity.mapToGerecht();
     }
 
+    @Put('gerechten/:id')
+    @ApiOperation({ title: 'Wijzig de gegevens van een gerecht' })
+    @ApiResponse({ status: 200, description: 'Het gerecht is bijgewerkt' })
+    @ApiResponse({ status: 404, description: 'Een gerecht met dit id is niet gevonden' })
+    @ApiResponse({ status: 409, description: 'Een gerecht met deze naam bestaat al' })
+    modifyGerecht(@Param('id') id: number, @Body() gerechtDto: CreateGerechtDto): Promise<UpdateResult> {
+        return this.gerechtService.updateGerecht(id, gerechtDto.mapToGerechtEntity());
+    }
+
     @Delete('gerechten/:id')
+    @ApiOperation({ title: 'Verwijder een gerecht' })
+    @ApiResponse({ status: 200, description: 'Het gerecht is verwijderd' })
+    @ApiResponse({ status: 404, description: 'Een gerecht met dit id is niet gevonden' })
     deleteGerecht(@Param('id') id: number): Promise<DeleteResult> {
         return this.gerechtService.deleteGerecht(id);
     }
 
     @Get('ingredienten')
+    @ApiOperation({ title: 'Haal een lijst op van alle ingredienten' })
     async getIngredienten(): Promise<{ ingredienten: Ingredient[] }> {
         const ingredienten = await this.ingredientenService.getIngredienten();
         return { ingredienten };
     }
 
     @Post('ingredienten')
+    @ApiOperation({ title: 'Voeg een nieuw ingredient toe' })
+    @ApiResponse({ status: 201, description: 'Het ingredient is succesvol aangemaakt' })
+    @ApiResponse({ status: 409, description: 'Een ingredient met deze naam bestaat al' })
     async createIngredient(@Body() ingredientDto: CreateIngredientDto): Promise<Ingredient> {
         const ingredientEntity = await this.ingredientenService.createIngredient(ingredientDto.mapToIngredientEntity());
         return ingredientEntity.mapToIngredient();
     }
 
+    @Put('ingredienten/:id')
+    @ApiOperation({ title: 'Wijzig de gegevens van een ingredient' })
+    @ApiResponse({ status: 200, description: 'Het ingredient is bijgewerkt' })
+    @ApiResponse({ status: 404, description: 'Een ingredient met dit id is niet gevonden' })
+    @ApiResponse({ status: 409, description: 'Een ingredient met deze naam bestaat al' })
+    modifyIngredient(@Param('id') id: number, @Body() ingredientDto: CreateIngredientDto): Promise<UpdateResult> {
+        return this.ingredientenService.updateIngredient(id, ingredientDto.mapToIngredientEntity());
+    }
+
     @Delete('ingredienten/:id')
+    @ApiOperation({ title: 'Verwijder een ingredient' })
+    @ApiResponse({ status: 200, description: 'Het ingredient is verwijderd' })
+    @ApiResponse({ status: 404, description: 'Een ingredient met dit id is niet gevonden' })
     deleteIngredient(@Param('id') id: number): Promise<DeleteResult> {
         return this.ingredientenService.deleteIngredient(id);
     }
--- backend/src/services/gerecht/gerecht.service.ts
+++ backend/src/services/gerecht/gerecht.service.ts
@@ -3,7 +3,7 @@ import { Gerecht } from 'src/models/gerecht';
 import { CreateGerechtDto } from 'src/dto/create-gerecht-dto';
 import { GerechtRepoEntity } from 'src/entities/gerecht.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, DeleteResult } from 'typeorm';
+import { Repository, DeleteResult, UpdateResult, Not } from 'typeorm';
 
 @Injectable()
 export class GerechtService {
@@ -28,7 +28,20 @@ export class GerechtService {
         }
     }
 
-    deleteGerecht(id: number): Promise<DeleteResult> {
-        return this.gerechtRepository.delete({id});
+    async updateGerecht(id: number, gerechtEntity: GerechtRepoEntity): Promise<UpdateResult> {
+        const gerechtExists = !!(await this.gerechtRepository.findOne({ id }));
+        if (!gerechtExists) throw new HttpException('Er bestaat geen gerecht met dit id', HttpStatus.NOT_FOUND);
+
+        const naamExists = !!(await this.gerechtRepository.findOne({ id: Not(id), naam: gerechtEntity.naam }));
+        if (naamExists) throw new HttpException('Een gerecht met deze naam bestaat al', HttpStatus.CONFLICT);
+
+        return this.gerechtRepository.update({ id }, gerechtEntity);
+    }
+
+    async deleteGerecht(id: number): Promise<DeleteResult> {
+        const gerechtExists = !!(await this.gerechtRepository.findOne({ id }));
+        if (!gerechtExists) throw new HttpException('Er bestaat geen gerecht met dit id', HttpStatus.NOT_FOUND);
+
+        return this.gerechtRepository.delete({ id });
     }
 }
