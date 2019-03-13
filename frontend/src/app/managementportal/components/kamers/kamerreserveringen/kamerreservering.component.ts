5d426a4adc4777cab72c591cec3cc6aa9cb5b182
Powercommy <sander@jemag.nl>
2019-03-13 11:18:42 +0100

    Cleaned some stuff

--- frontend/src/app/managementportal/components/kamers/kamerreserveringen/kamerreservering.component.ts
+++ frontend/src/app/managementportal/components/kamers/kamerreserveringen/kamerreservering.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { KamerreserveringenService } from 'src/app/services/kamerreserveringen.service';
 import { take, tap } from 'rxjs/operators';
 import { KamerReservering } from 'src/app/models/kamerreservering';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import { Router } from '@angular/router';
 import {FormKamerreserveringdetailsComponent} from "../kamers-form/form-kamerreserveringdetails/form-kamerreserveringdetails.component";
 
@@ -13,6 +13,9 @@ import {FormKamerreserveringdetailsComponent} from "../kamers-form/form-kamerres
 })
 export class KamerreserveringComponent implements OnInit {
   kamerreserveringen: KamerReservering[] | undefined = [];
+  public selectedResevering?: KamerReservering;
+  closeResult: string = "";
+
   constructor(private readonly kamerreserveringservice: KamerreserveringenService, private readonly modalService: NgbModal, private router: Router) { }
 
   field: string = "";
@@ -39,11 +42,38 @@ export class KamerreserveringComponent implements OnInit {
       modalKamerReservering.componentInstance.kamerReservering = kamerReservering;
     }
   }
+
+  onSelect(kamerresevering: KamerReservering): void {
+    this.selectedResevering = kamerresevering;
+  }
+
+
+  openSm(content: NgbModal) {
+    this.modalService
+      .open(content, { ariaLabelledBy: "modal-basic-title" })
+      .result.then(
+      result => {
+        this.closeResult = `Closed with: ${result}`;
+      },
+      reason => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      }
+    );
+  }
+
   deleteRoom(kamerdata: KamerReservering) {
     if (kamerdata) {
       this.kamerreserveringservice.deleteKamerReservering(kamerdata);
-      //this.kamers = [...this.kamers].filter(item => item !== kamer);
     }
   }
 
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return "by pressing ESC";
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return "by clicking on a backdrop";
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 }
