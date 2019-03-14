e675c70e065f71f584347f6647c81074b8f9dae7
Powercommy <sander@jemag.nl>
2019-03-14 14:36:54 +0100

    knop

--- frontend/src/app/services/kamerreserveringen.service.ts
+++ frontend/src/app/services/kamerreserveringen.service.ts
@@ -10,6 +10,7 @@ import {map, take, tap} from 'rxjs/operators';
 export class KamerreserveringenService {
   private kamerReseveringCacheSubject = new BehaviorSubject<KamerReservering[] | undefined>(undefined);
   private static api = `/api/kamerreservering`;
+
   constructor(private http: HttpClient) {}
 
   getKamerReserveringen(refreshCache: boolean = false): Observable<KamerReservering[] | undefined> {
@@ -29,6 +30,40 @@ export class KamerreserveringenService {
     return this.kamerReseveringCacheSubject;
   }
 
+  getKamerToekomstReserveringen(refreshCache: boolean = false): Observable<KamerReservering[] | undefined> {
+    console.log(this.getCurrentDate());
+    if (this.kamerReseveringCacheSubject.getValue() === undefined || refreshCache) {
+      this.http.get<IKamerReservering>(`${KamerreserveringenService.api}/actief/${this.getCurrentDate()}`)
+        .pipe(
+          map((data: any) => data.map((kamerreservering: KamerReservering) =>new KamerReservering(kamerreservering.id,
+            kamerreservering.voornaam, kamerreservering.achternaam, kamerreservering.telefoonnummer, kamerreservering.emailadres,
+            kamerreservering.identiteitsid, kamerreservering.postcode, kamerreservering.straat, kamerreservering.huisnummer,
+            kamerreservering.woonplaats, kamerreservering.land, kamerreservering.datumvan, kamerreservering.datumtot, kamerreservering.kamernaam))),
+          take(1),
+          tap(kamerreservering => {
+            this.kamerReseveringCacheSubject.next(kamerreservering);
+          })
+        ).subscribe()
+    }
+    return this.kamerReseveringCacheSubject;
+  }
+
+  getKamerVerledenReserveringen(refreshCache: boolean = false): Observable<KamerReservering[] | undefined> {
+    if (this.kamerReseveringCacheSubject.getValue() === undefined || refreshCache) {
+      this.http.get<IKamerReservering>(`${KamerreserveringenService.api}/inactief/${this.getCurrentDate()}`)
+        .pipe(
+          map((data: any) => data.map((kamerreservering: KamerReservering) =>new KamerReservering(kamerreservering.id,
+            kamerreservering.voornaam, kamerreservering.achternaam, kamerreservering.telefoonnummer, kamerreservering.emailadres,
+            kamerreservering.identiteitsid, kamerreservering.postcode, kamerreservering.straat, kamerreservering.huisnummer,
+            kamerreservering.woonplaats, kamerreservering.land, kamerreservering.datumvan, kamerreservering.datumtot, kamerreservering.kamernaam))),
+          take(1),
+          tap(kamerreservering => {
+            this.kamerReseveringCacheSubject.next(kamerreservering);
+          })
+        ).subscribe()
+    }
+    return this.kamerReseveringCacheSubject;
+  }
   saveKamerReservering(kamerreservering: KamerReservering){
     this.http.post(`${KamerreserveringenService.api}`, kamerreservering)
       .pipe(
@@ -43,6 +78,24 @@ export class KamerreserveringenService {
         take(1),
         tap(() => this.getKamerReserveringen(true))
       ).subscribe();
+  }
+
+  getCurrentDate(): string{
+    const currentDate:Date = new Date();
+    let today: string = "";
+    let dd:any = currentDate.getDate();
+    let mm:any = currentDate.getMonth()+1;
+    let yyyy:any = currentDate.getFullYear();
+
+    if(dd<10) {
+      dd = '0'+dd
+    }
+
+    if(mm<10) {
+      mm = '0'+mm
+    }
+    today = (yyyy + '-' + mm + '-' + dd);
+    return today;
   }
 
 }
