8cda715542b5cd8a85aa71ef0d145869e7a73ce0
Jilles Wielaard <jilles@wielaard.nu>
2019-03-15 10:50:52 +0100

    import change

--- backend/src/models/gerecht-details.ts
+++ backend/src/models/gerecht-details.ts
@@ -1,14 +1,14 @@
-import { GerechtType } from "src/enums/gerechttype";
-import { GerechtSubType } from "src/enums/gerechtsubtype";
-import { Ingredient } from "dist/models/ingredient";
+import { GerechtType } from 'src/enums/gerechttype';
+import { GerechtSubType } from 'src/enums/gerechtsubtype';
+import { Ingredient } from './ingredient';
 
 export class GerechtDetails {
-    constructor(
-        public readonly id: number,
-        public readonly naam: string,
-        public readonly type: GerechtType,
-        public readonly subtype: GerechtSubType,
-        public readonly prijs: number,
-        public readonly ingredienten: Ingredient[]
-    ) { }
+  constructor(
+    public readonly id: number,
+    public readonly naam: string,
+    public readonly type: GerechtType,
+    public readonly subtype: GerechtSubType,
+    public readonly prijs: number,
+    public readonly ingredienten: Ingredient[],
+  ) {}
 }
