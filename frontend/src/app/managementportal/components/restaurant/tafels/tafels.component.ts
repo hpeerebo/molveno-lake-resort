f90d935a52bf7702ade43f1c30ae4cf5b5e68540
Jilles <jilles.wielaard@kpn.com>
2019-03-27 14:49:06 +0100

    refactor

--- frontend/src/app/managementportal/components/restaurant/tafels/tafels.component.ts
+++ frontend/src/app/managementportal/components/restaurant/tafels/tafels.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Tafel } from 'src/app/models/tafel';
 import { TafelsService } from 'src/app/services/tafels.service';
@@ -11,50 +11,60 @@ import { ModalConfirmComponent } from 'src/app/shared/components/modal-confirm/m
   templateUrl: './tafels.component.html',
   styleUrls: ['./tafels.component.scss']
 })
-export class ManagementPortalTafelsComponent {
+export class ManagementPortalTafelsComponent implements OnInit {
   public tafels$: Observable<Tafel[]> = this.tafelsService.data$;
 
-  field: string = "kenmerk";
-  public clickColumnHandler(event: string): string {
-    this.field = event;
-    return console.log(this.field), this.field;
-  }
+  public currentSelectedColumn: string | undefined = undefined;
 
   constructor(
     private tafelsService: TafelsService,
     private modalService: NgbModal,
-    ) {}
+  ) { }
 
-  openFormTafelModal(tafel?: Tafel) {
+  ngOnInit() {
+    this.handleColumnHeaderClick('kenmerk');
+  }
+
+  public handleColumnHeaderClick(event: 'kenmerk' | 'personen'): string {
+    this.currentSelectedColumn = event;
+    return this.currentSelectedColumn;
+  }
+
+  public handleNewTableButtonClick() {
+    this.openFormTafelModal();
+  }
+
+  public handleModifyTableButtonClick(tafel: Tafel) {
+    this.openFormTafelModal(tafel);
+  }
+
+  public handleDeleteTableButtonClick(tafel: Tafel) {
+    this.verwijderTafel(tafel);
+  }
+
+  private async openFormTafelModal(tafel?: Tafel) {
     const modal = this.modalService.open(FormTafelComponent);
 
     if (tafel) {
       modal.componentInstance.tafel = tafel;
     }
 
-    modal.result
-      .then(result => {
-        if (result.id) {
-          this.tafelsService.updateTafel(result);
-        } else {
-          this.tafelsService.addNewTafel(result);
-        }
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    try {
+      const result = await modal.result
+      if (result.id) {
+        this.tafelsService.updateTafel(result);
+      } else {
+        this.tafelsService.addNewTafel(result);
+      }
+    } catch (message) { }
   }
 
-  verwijderTafel(tafel: Tafel) {
-    this.modalService
-      .open(ModalConfirmComponent)
-      .result.then(result => {
-        if (result === 'yes') {
-          this.tafelsService.deleteTafel(tafel);
-        }
-      })
-      .catch(error => {
-        console.log(error);
-      });
+  private async verwijderTafel(tafel: Tafel) {
+    try {
+      const result = await this.modalService.open(ModalConfirmComponent).result
+      if (result === 'yes') {
+        this.tafelsService.deleteTafel(tafel);
+      }
+    } catch (message) { }
   }
 }
